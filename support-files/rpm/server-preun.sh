0492f01ab2e0dd2a6fdf02ad55ff62a8a1b80562
david hill <david.hill@mariadb.com>
2016-05-15 13:11:39 -0500

    change mysql name

--- support-files/rpm/server-preun.sh
+++ support-files/rpm/server-preun.sh
@@ -1,11 +1,7 @@
 if [ $1 = 0 ] ; then
 	# Stop MySQL before uninstalling it
-	if [ -x %{_sysconfdir}/init.d/mysql ] ; then
-		%{_sysconfdir}/init.d/mysql stop > /dev/null
-	fi
-        # Don't start it automatically anymore
-	if [ -x /sbin/chkconfig ] ; then
-		/sbin/chkconfig --del mysql
+	if [ -x %{_sysconfdir}/init.d/mysql-Columnstore ] ; then
+		%{_sysconfdir}/init.d/mysql-columnstore stop > /dev/null
 	fi
 fi
 
