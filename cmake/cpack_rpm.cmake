6f93270865b9e00ac4afbea2f137b390d0b94261
david hill <david.hill@mariadb.com>
2016-06-06 20:24:16 +0200

    more directory changes

--- cmake/cpack_rpm.cmake
+++ cmake/cpack_rpm.cmake
@@ -96,7 +96,6 @@ SET(ignored
   )
 
 SET(CPACK_RPM_server_USER_FILELIST ${ignored} "%config(noreplace) ${INSTALL_SYSCONF2DIR}/*")
-SET(CPACK_RPM_common_USER_FILELIST ${ignored} "%config(noreplace) ${INSTALL_SYSCONFDIR}/my.cnf")
 SET(CPACK_RPM_shared_USER_FILELIST ${ignored} "%config(noreplace) ${INSTALL_SYSCONF2DIR}/*")
 SET(CPACK_RPM_client_USER_FILELIST ${ignored} "%config(noreplace) ${INSTALL_SYSCONF2DIR}/*")
 SET(CPACK_RPM_compat_USER_FILELIST ${ignored})
